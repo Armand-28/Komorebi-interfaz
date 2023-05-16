2f221dcfbf4c5666b5ecb7b537f7413b334e9013
Juan Diego Valderrama Rodriguez <jdvalderrama68@misena.edu.co>
2023-05-16 11:58:41 -0500

    avance 16-05-23

--- recibir.php
+++ recibir.php
@@ -2,7 +2,14 @@
 include("conexion.php");
 
 $consulta = "SELECT * FROM productos";
-$execute = mysqli_query($conectar, $consulta) or die ("Error").mysqli_error($conexion);
- 
+$execute = mysqli_query($conexion, $consulta) or die ("Error").mysqli_error($conexion);
 
+while($mostrar = mysqli_fetch_array($execute)){
+    $mostrar['cliname'];
+    $mostrar['cliemail'];
+    $mostrar['clinumcel'];
+    $mostrar['cligenero'];
+    $mostrar['clifecnac'];
+    $mostrar['clicontra'];
+}
 ?>
