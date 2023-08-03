c8c46ba7f2981db8272f6e42d6f6904554a27f8b
Ndobito <jcuellarmenza@gmail.com>
2023-08-03 17:05:32 -0500

    MVC Inicio Index

--- MVC/controller/index.controller.php
+++ MVC/controller/index.controller.php
@@ -1,13 +1,18 @@
 <?php
 
+include_once "model/index.php"; 
+
 class IndexController{
 
-    public function __construct(){
+    private $object; 
 
+    public function __construct(){
+        $this-> object = new Index(); 
     }
 
     public function Inicio(){
-        
+        $style = "<link rel='stylesheet' type='text/css' href='assets/css/style-index.css'>"; 
+        require_once "view/index/index.php"; 
     }
 
 }
