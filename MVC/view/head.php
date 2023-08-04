0990088ae79e1372a0a052d30bc11d3870203560
JJherrera7 <juanpablo.herreratorres.79@gmail.com>
2023-08-04 10:38:24 -0500

    carrucel

--- MVC/view/head.php
+++ MVC/view/head.php
@@ -6,4 +6,6 @@
     <title>Komobrebi</title>
     <?php echo (isset($style)) ? $style : "" ?>
     <script src="https://kit.fontawesome.com/96f9a4100d.js" crossorigin="anonymous"></script>
+    <!-- Librerias de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
