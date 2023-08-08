926da9a3acb1b1baaa69c8854e478362d010f8dc
Armand-28 <armandopp2809@gamil.com>
2023-08-08 09:45:09 -0500

    carrusel

--- MVC/view/head.php
+++ MVC/view/head.php
@@ -7,5 +7,5 @@
     <?php echo (isset($style)) ? $style : "" ?>
     <script src="https://kit.fontawesome.com/96f9a4100d.js" crossorigin="anonymous"></script>
     <!-- Librerias de Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">    
 </head>
