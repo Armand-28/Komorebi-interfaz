1c28d1ecaed8f6e7cdcddba97477581657218194
Armand-28 <armandopp2809@gamil.com>
2023-08-15 14:25:06 -0500

    15 agoust

--- MVC/assets/js/index.js
+++ MVC/assets/js/index.js
@@ -1,9 +1,9 @@
 // codigo para hacer aparecer y desaparecer el carrito
 
+function mostrarCarrito(){
+    document.getElementById("carrito").style.display = "block";
+}
 
-var aparecerCarrito = document.getElementsByClassName("micarro");
-
-aparecerCarrito.addEventListener("click", () =>{
-    document.getElementById("carrito").style.display = "block"
-
-})
+function ocultarCarrito(){
+    document.getElementById("carrito").style.display = "none";
+}
